b7f01b7e613ce6885edefc05bfaad85c7c0b0a6b
sbryant1014 <sbryant1014@gmail.com>
2015-09-01 13:32:13 -0400

    Removed old code from Utilties.java

--- src/core/Utilities.java
+++ src/core/Utilities.java
@@ -11,11 +11,6 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
-import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
 
 /**
  *
@@ -23,82 +18,6 @@ import java.util.List;
  */
 public class Utilities {
   
-  public static <T> String getArrayString(List<T> list) {
-    String str = "";
-    for (int i = 0; i < list.size(); i++) {
-      if (i < list.size() - 1) {
-        str += list.get(i).toString() + ",";
-      } else {
-        str += list.get(i);
-      }
-    }
-    return str;
-  }
-  
-  public static <T> String getCollectionString(Collection<T> list) {
-    String str = "";
-    Iterator<T> iter = list.iterator();
-    while (iter.hasNext()) {
-      Object o = iter.next();
-      if (iter.hasNext()) {
-        str += o.toString() + ",";
-      } else {
-        str += o.toString();
-      }
-    }
-    return str;
-  }
-  
-  public static Object getObjectFieldValue(Object obj, String fieldName) throws IllegalArgumentException, NoSuchFieldException, IllegalAccessException {
-    Field f = obj.getClass().getField(fieldName);
-    return f.get(obj);
-  }
-  
-  public static void setObjectFieldValue(Object obj, String fieldName, Object value) throws NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
-    Field f = obj.getClass().getField(fieldName);
-    f.set(obj, value);
-  }
-  
-  public static String encodeObjectStringField(Object obj, String fieldName) throws NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
-    return String.format("%s:%s\n", fieldName, getObjectFieldValue(obj, fieldName).toString());
-  }
-  
-  
-  
-  public static String encodeObjectListField(Object obj, String fieldName) throws NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
-    List<Object> list = (List<Object>) getObjectFieldValue(obj, fieldName);
-    String str = "";
-    for (int i = 0; i < list.size(); i++) {
-      if (i < list.size() - 1) {
-        str += list.get(i).toString() + ",";
-      } else {
-        str += list.get(i);
-      }
-    }
-    return str + "\n";
-  }
-  
-  public static void decodeObjectStringField(Object obj, String line) throws NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
-    String parts[] = line.split(":");
-    String fieldName = parts[0];
-    setObjectFieldValue(obj, parts[0], parts[1]);
-  }
-  
-//  public static void decodeObjectListField(Object obj, String line) throws NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
-//    String parts[] = line.split(":");
-//    String fieldName = parts[0];
-//    String[] list = 
-//    
-//    setObjectFieldValue(obj, parts[0], parts[1]);
-//  }
-//  
-//  private String _encodeList(String fieldName) {
-//    List<Object> list = (List<Object>) this._getValue(fieldName);
-//    String str = "";
-//    
-//    return String.format("%s:%s\n", fieldName, this._getValue(fieldName));
-//  }
-//  
   public static <T extends Serializable> T makeDeepCopy(T object) {
     try {
       ByteArrayOutputStream bos = new ByteArrayOutputStream();
