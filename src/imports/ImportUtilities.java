e5b6a9cdf36aa29fe52e89d51b25f2068aa6931c
sbryant1014 <sbryant1014@gmail.com>
2015-09-01 17:29:35 -0400

    IMP: Added real data import for PT1

---
+++ src/imports/ImportUtilities.java
@@ -0,0 +1,27 @@
+package imports;
+
+import core.IO;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import models.Answer;
+
+public class ImportUtilities {
+  
+  static Map<Integer, Answer> readAnswerFile(String filename) {
+    Map<Integer, Answer> map = new HashMap<>();
+    
+    Scanner scan = IO.getScannerOrDie(filename);
+    String line;
+    while (scan.hasNextLine()) {
+      line = scan.nextLine().trim();
+      if (line.compareTo("") != 0) {
+        String[] parts = line.split(" ");
+        System.out.printf("Parts: |%s| |%s|\n", parts[0], parts[1]);
+        map.put(Integer.parseInt(parts[0]), Answer.valueOf(parts[1]));        
+      }
+    }
+    return map;
+  }
+  
+}
