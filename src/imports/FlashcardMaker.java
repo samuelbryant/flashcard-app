4378afef3a55c4af610c60db458177043256046c
sbryant1014 <sbryant1014@gmail.com>
2015-09-13 18:25:33 -0400

    Made huge improvements to flashcard importer

--- src/imports/FlashcardMaker.java
+++ src/imports/FlashcardMaker.java
@@ -7,6 +7,7 @@ import core.IO;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
@@ -18,6 +19,7 @@ import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JButton;
@@ -36,13 +38,16 @@ public class FlashcardMaker extends JPanel implements MouseListener {
   Dimension forcedDim = new Dimension(800, 800);
   double resizeRatio;
   
+  static int CLOSE_THRESHOLD = 20;
+  
   // Dimension dim = new Dimension(5100, 6600);
   BufferedImage img;
   Integer saveIndex = 0;
   File[] images;
   Integer imageIndex = -1;
   
-  int verticalSplit = -1;
+  int verticalLeft = -1;
+  int verticalRight = -1;
   ArrayList<Integer> horizontalSplits = new ArrayList<>();
   
   public FlashcardMaker(File[] images) {
@@ -61,7 +66,8 @@ public class FlashcardMaker extends JPanel implements MouseListener {
     this.setPreferredSize(dim);
     this.setSize(dim);
     resizeRatio = ((double) forcedDim.width) / img.getWidth();
-    this.verticalSplit = -1;
+    this.verticalLeft = -1;
+    this.verticalRight = -1;
     this.horizontalSplits.clear();
     this.repaint();
   }
@@ -81,17 +87,25 @@ public class FlashcardMaker extends JPanel implements MouseListener {
         (int) (width*resizeRatio), (int) (height*resizeRatio), null);
     // gr.drawImage(img, 0, 0, null);
     
-    if (this.verticalSplit != -1) {
-      gr.setColor(Color.RED);
-      int x = (int) (this.verticalSplit*resizeRatio);
-      gr.drawLine(x, 0, x, h);
-    }
+    this.drawVertical(gr);
+    
     for (Integer i : this.horizontalSplits) {
       int y = (int) (i * resizeRatio);
       gr.drawLine(0, y, w, y);
     }
     
-    
+  }
+  
+  private void drawVertical(Graphics gr) {
+    if (this.verticalLeft != -1 && this.verticalRight != -1) {
+      int left = (int) (verticalLeft*resizeRatio);
+      int right = (int) (verticalRight*resizeRatio);
+      gr.setColor(Color.RED);
+      gr.fillRect(left, 0, right-left, this.getHeight());
+    } else if (this.verticalLeft != -1) {
+      gr.setColor(Color.RED);
+      gr.drawLine((int) (verticalLeft*resizeRatio), 0, (int) (verticalLeft*resizeRatio), this.getHeight());
+    }
   }
   
   @Override
@@ -106,21 +120,38 @@ public class FlashcardMaker extends JPanel implements MouseListener {
     x = (int) (x/resizeRatio);
     y = (int) (y/resizeRatio);
     
-    System.out.printf("Clicked: (%d, %d) (%d, %d)\n", x, y, this.getWidth(), this.getHeight());
-    if (this.verticalSplit == -1) {
-      verticalSplit = x;
+    this.addClick(x, y);
+    
+    this.repaint();
+  }
+  
+  private void undoClick() {
+    if (!this.horizontalSplits.isEmpty()) {
+      this.horizontalSplits.remove(this.horizontalSplits.size() - 1);
+    } else if (verticalRight != -1) {
+      this.verticalRight = -1;
+    } else {
+      this.verticalRight = -1;
+    }
+  }
+  
+  private void addClick(int x, int y) {
+    if (verticalLeft == -1) {
+      verticalLeft = x;
+    } else if (verticalRight == -1) {
+      verticalRight = x;
     } else {
       if (!this.horizontalSplits.isEmpty()) {
         int last = this.horizontalSplits.get(this.horizontalSplits.size() - 1);
         int diff = y - last;
-        if (diff < 5) {
-          System.err.printf("Ignoring click within 5 of last click\n");
+        if (diff <= CLOSE_THRESHOLD) {
+          System.err.printf("Detected close clicks!\n");
         }
         
       }
       this.horizontalSplits.add(y);
+      Collections.sort(this.horizontalSplits);
     }
-    this.repaint();
   }
 
   @Override
@@ -143,14 +174,21 @@ public class FlashcardMaker extends JPanel implements MouseListener {
     DatabaseIO<Flashcard> io = DatabaseIO.getFlashcardDatabaseIO();
     Database<Flashcard> db = io.get();
     
-    int mid = this.verticalSplit;
+    int midL = this.verticalLeft;
+    int midR = this.verticalRight;
     int right = img.getWidth();
     int top = this.horizontalSplits.get(0);
     for (int i=1; i<this.horizontalSplits.size(); i++) {
       int bot = this.horizontalSplits.get(i);
       
-      BufferedImage qImgPart = img.getSubimage(0, top, mid, bot-top);
-      BufferedImage aImgPart = img.getSubimage(mid, top, right-mid, bot-top);
+      if (Math.abs(bot - top) <= CLOSE_THRESHOLD) {
+        System.out.printf("Skipping close clicks (%d)\n", bot-top);
+        top = bot;
+        continue;
+      }
+      
+      BufferedImage qImgPart = img.getSubimage(0, top, midL, bot-top);
+      BufferedImage aImgPart = img.getSubimage(midR, top, right-midR, bot-top);
       
       int qWidth = qImgPart.getWidth();
       int qHeight = qImgPart.getHeight();
@@ -217,12 +255,24 @@ public class FlashcardMaker extends JPanel implements MouseListener {
         }
       });
       
+      JButton undoButton = new JButton("Undo");
+      undoButton.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+          undoClick();
+        }
+      });
+      
+      JPanel buttonPanel = new JPanel();
+      buttonPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 10, 10));
+      buttonPanel.add(saveButton);
+      buttonPanel.add(undoButton);
       //JScrollPane pane = new JScrollPane(FlashcardMaker.this);
       //pane.setPreferredSize(new Dimension(800, 800));
       
       
       this.getContentPane().add(FlashcardMaker.this, BorderLayout.CENTER);
-      this.getContentPane().add(saveButton, BorderLayout.SOUTH);
+      this.getContentPane().add(buttonPanel, BorderLayout.SOUTH);
       
       this.pack();
       this.setVisible(true);
